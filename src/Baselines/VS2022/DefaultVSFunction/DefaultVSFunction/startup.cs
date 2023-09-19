22af3f8e0541b44a293510dffea0b05091e22598
Nicholas Rogoff <nicholas.rogoff@avanade.com>
2023-09-19 16:55:42 +0100

    Added a startup to the VS2022 Default Function

---
+++ src/Baselines/VS2022/DefaultVSFunction/DefaultVSFunction/startup.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(DefaultVSFunction.Startup))]
+
+namespace DefaultVSFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            IConfiguration configuration = builder.Services.BuildServiceProvider()
+                .GetService<IConfiguration>();
+            string myAppSetting = configuration["MyAppSetting"];
+            // use myAppSetting or any other configuration value you need
+            // add your services here
+        }
+    }
+}
