96123655d7ac8ea55e867a11a98611defc9c6cc1
Nicholas Rogoff <nicholas.rogoff@avanade.com>
2023-09-19 16:58:32 +0100

    Everything working

--- src/UnitTestSampleAPIFunction/UnitTestSampleAPIFunction/UnitTestSampleAPIFunction/Functions/GetCustomers.cs
+++ src/UnitTestSampleAPIFunction/UnitTestSampleAPIFunction/UnitTestSampleAPIFunction/Functions/GetCustomers.cs
@@ -37,23 +37,38 @@ namespace UnitTestSampleAPIFunction.Functions
         /// Get Customers API. This is a sample API to demonstrate how to write unit tests for Azure Functions. It simulates some of the features of OData, but is not using the OData SDK.
         /// </summary>
         /// <param name="req"></param>
+        /// <param name="logger"></param>
         /// <returns></returns>
         [FunctionName("GetCustomers")]
         // Add these OpenAPI attributes
-        [OpenApiOperation(operationId: "GetCustomers", tags: new[] { "customers" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "top", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "The number of records it wants to retrieve in each page")]
-        [OpenApiParameter(name: "skip", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "The offset from the beginning of the results")]
-        [OpenApiParameter(name: "filter", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "used to filter the results of a data query, so that only a subset of the data that satisfies the specified condition is returned.")]
-        [OpenApiParameter(name: "count", In = ParameterLocation.Query, Required = false, Type = typeof(bool), Description = "Enable the count to be returned. Default = true")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(CustomerListDto), Description = "The list of Customers, Total Count and Link to the next page.")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.NoContent, contentType: "application/json", bodyType: typeof(CustomerListDto), Description = "The request was not understood. Possibly a parameter is missing or incorrect.")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "No customers matching the parameters could be found.")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: "application/json", bodyType: typeof(Exception), Description = "Internal Server Error.")]
+        [OpenApiOperation(operationId: "GetCustomers", tags: new[] { "customers" }, Summary = "This returns a collection of Customers")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key",
+            In = OpenApiSecurityLocationType.Header, Description = "This is the Azure Function API Key. In production this should be hidden behind API Manager")]
+        [OpenApiParameter(name: "top", In = ParameterLocation.Query, Required = false, Type = typeof(int),
+            Description = "The number of records it wants to retrieve in each page")]
+        [OpenApiParameter(name: "skip", In = ParameterLocation.Query, Required = false, Type = typeof(int),
+            Description = "The offset from the beginning of the results")]
+        [OpenApiParameter(name: "filter", In = ParameterLocation.Query, Required = false, Type = typeof(string),
+            Description =
+                "used to filter the results of a data query, so that only a subset of the data that satisfies the specified condition is returned.")]
+        [OpenApiParameter(name: "count", In = ParameterLocation.Query, Required = false, Type = typeof(bool),
+            Description = "Enable the count to be returned. Default = true")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json",
+            bodyType: typeof(CustomerListDto),
+            Description = "The list of Customers, Total Count and Link to the next page.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.NoContent, contentType: "application/json",
+            bodyType: typeof(CustomerListDto),
+            Description = "The request was not understood. Possibly a parameter is missing or incorrect.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json",
+            bodyType: typeof(string), Description = "No customers matching the parameters could be found.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: "application/json",
+            bodyType: typeof(Exception), Description = "Internal Server Error.")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger logger)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, ILogger logger)
         {
             _logger = logger;
+
             #region Get the parameters from the query string
 
             // Each parameter has a default value in the configuration file.
@@ -61,7 +76,7 @@ namespace UnitTestSampleAPIFunction.Functions
             // If the parameter passed in the query string is not valid, the default value is used.
 
             int top;
-            if(!int.TryParse(req.Query["top"], out top))
+            if (!int.TryParse(req.Query["top"], out top))
             {
                 top = _config.GetValue<int>("DefaultTop");
             }
@@ -91,29 +106,32 @@ namespace UnitTestSampleAPIFunction.Functions
             {
                 orderBy = req.Query["orderby"];
             }
-            
+
             bool count;
             if (!bool.TryParse(req.Query["count"], out count))
             {
                 count = _config.GetValue<bool>("DefaultCount");
             }
-            
+
             #endregion
 
 
-            #region Get the parameters from the request body
+            #region Get the parameters from the request body if using a POST
 
             //get the parameters from the request body and overwrite the defaults if they are passed in
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            top = data?.top;
-            skip = data?.skip;
-            filter = data?.filter;
-            orderBy = data?.orderby;
-            count = data?.count;
+            if (data != null)
+            {
+                top = data.top;
+                skip = data.skip;
+                filter = data.filter;
+                orderBy = data.orderby;
+                count = data.count;
+            }
 
             #endregion
-            
+
 
             #region Application Insights Logging and setting up for timing and metrics
 
@@ -125,15 +143,23 @@ namespace UnitTestSampleAPIFunction.Functions
             };
             // Create an Event Id
             var eventId = new EventId(1, "GetCustomers");
-            _logger.Log(LogLevel.Information, eventId,"GetCustomer Function Called", logProps);
+            _logger.Log(LogLevel.Information, eventId, "GetCustomer Function Called", logProps);
             // or use the explicit log level
-            _logger.LogInformation(eventId,"GetCustomer Function Called", logProps);
+            _logger.LogInformation(eventId, "GetCustomer Function Called", logProps);
 
             //Log some Metrics
-            _logger.LogMetric("RecordsRequests",Convert.ToDouble(top));
+            _logger.LogMetric("RecordsRequests", Convert.ToDouble(top));
 
             #endregion
 
+            #region Get the data from the database
+
+            // Get the data from the database using the top, skip, filter and orderBy parameters
+            // Because handling a full featured filter is beyond the scope of this sample, we are only handling a simple filter on the Customer Surname
+            
+            
+            
+            #endregion
 
             string responseMessage = "This HTTP triggered function executed successfully.";
 
